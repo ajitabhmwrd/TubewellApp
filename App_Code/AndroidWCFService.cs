5ad90a4a3892dc544e72b4e67fd5dd89d9baf9ad
Ajitabh kumar <ajitabh2007@gmail.com>
2020-03-19 19:11:51 +0530

    webservice

---
+++ App_Code/AndroidWCFService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AndroidWCFService" in code, svc and config file together.
+
+public class AndroidWCFService : IAndroidWCFService
+{
+    getData gd = new getData();
+
+    public AppDetail getAppDetail()
+    {
+        try
+        {
+            DataTable dt = gd.getDataTable("getAppDetail");
+            if (dt.Rows.Count > 0)
+            {
+                return new AppDetail
+                {
+                    Priority = dt.Rows[0]["Priority"].ToString(),
+                    AdminMsg = dt.Rows[0]["AdminMsg"].ToString(),
+                    AdminTitle = dt.Rows[0]["AdminTitle"].ToString(),
+                    UpdateMsg = dt.Rows[0]["UpdateMsg"].ToString(),
+                    UpdateTitle = dt.Rows[0]["UpdateTitle"].ToString(),
+                    aapUrl = dt.Rows[0]["aapUrl"].ToString(),
+                    Role = dt.Rows[0]["Role"].ToString(),
+                    Ver = dt.Rows[0]["Ver"].ToString()
+                };
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+        return new AppDetail { };
+    }
+
+    public ClsLogin VerifyLogin(string UserID, string Password)
+    {
+        try
+        {
+
+            Encryptor enc = new Encryptor(Encryptor.PrivateKey);
+            SqlParameter[] prm = new SqlParameter[]
+            {
+                new SqlParameter("@LoginId",UserID),
+                new SqlParameter("@Pass",enc.Encrypt(Password))
+            };
+            DataTable dt = gd.getDataTable("getLoginDetails", prm);
+            if (dt.Rows.Count == 1)
+            {
+                string message = "Success";
+                bool status = true;
+                string RoleId = dt.Rows[0]["RoleId"].ToString();
+                string RoleName = dt.Rows[0]["RoleName"].ToString();
+                string ZoneCode = dt.Rows[0]["ZoneCode"].ToString();
+                string ZoneName = dt.Rows[0]["ZoneName"].ToString();
+                string DivisionCode = dt.Rows[0]["DivisionCode"].ToString();
+                string DivisionName = dt.Rows[0]["DivisionName"].ToString();
+                string DistCode = dt.Rows[0]["DistCode"].ToString();
+                string DistName = dt.Rows[0]["DistName"].ToString();
+                return new ClsLogin
+                {
+                    message = message,
+                    status = status,
+                    RoleId = RoleId,
+                    RoleName = RoleName,
+                    DistCode = DistCode != "" ? DistCode : null,
+                    DistName = DistName != "" ? DistName : null,
+                    ZoneCode = ZoneCode != "" ? ZoneCode : null,
+                    ZoneName = ZoneName != "" ? ZoneName : null,
+                    DivisionCode = DivisionCode != "" ? DivisionCode : null,
+                    DivisionName = DivisionName != "" ? DivisionName : null
+                };
+            }
+            else
+            {
+
+            }
+            return new ClsLogin { message = "Invalid Credentials, Please try again!!!", status = false };
+        }
+        catch (Exception ex)
+        {
+            return new ClsLogin { message = "Some error occured!!!", status = false };
+        }
+    }
+
+}
---
+++ App_Code/IAndroidWCFService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.Data;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAndroidWCFService" in both code and config file together.
+[ServiceContract]
+public interface IAndroidWCFService
+{
+    [OperationContract]
+    [WebInvoke(Method = "GET",
+        RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "getAppDetail")]
+    AppDetail getAppDetail();
+
+    [OperationContract]
+    [WebInvoke(Method = "POST", 
+        BodyStyle = WebMessageBodyStyle.WrappedRequest,
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "VerifyLogin")]
+    ClsLogin VerifyLogin(string UserID, string Password);
+}
+
+[DataContract]
+public class ClsLogin
+{
+    [DataMember]
+    public bool status { get; set; }
+    [DataMember]
+    public string message { get; set; }
+    [DataMember]
+    public string ZoneCode { get; set; }
+    [DataMember]
+    public string ZoneName { get; set; }
+    [DataMember]
+    public string DivisionCode { get; set; }
+    [DataMember]
+    public string DivisionName { get; set; }
+    [DataMember]
+    public string DistCode { get; set; }
+    [DataMember]
+    public string DistName { get; set; }
+    [DataMember]
+    public string RoleId { get; set; }
+    [DataMember]
+    public string RoleName { get; set; }
+}
+
+public class AppDetail
+{
+    
+    [DataMember]
+    public string Priority { get; set; }
+    [DataMember]
+    public string AdminMsg { get; set; }
+    [DataMember]
+    public string AdminTitle { get; set; }
+    [DataMember]
+    public string UpdateMsg { get; set; }
+    [DataMember]
+    public string UpdateTitle { get; set; }
+    [DataMember]
+    public string aapUrl { get; set; }
+    [DataMember]
+    public string Role { get; set; }
+    [DataMember]
+    public string Ver { get; set; }
+}
+
+
+
+
+
+
+
