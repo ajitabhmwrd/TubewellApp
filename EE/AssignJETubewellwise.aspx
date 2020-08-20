a86a1f7ade04ce3e6401e8880128b0c1231c41a5
Ajitabh kumar <ajitabh2007@gmail.com>
2020-08-20 14:18:58 +0530

    AddJETwWise

---
+++ EE/AssignJETubewellwise.aspx
@@ -0,0 +1,149 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EE/eeMaster.master" AutoEventWireup="true" CodeFile="AssignJETubewellwise.aspx.cs" Inherits="EE_AssignJETubewellwise" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <style type="text/css">
+        .modalBackground {
+            background-color: Black;
+            filter: alpha(opacity=90);
+            opacity: 0.8;
+        }
+
+        .modalPopup {
+            background-color: #FFFFFF;
+            border-color: black;
+            width: 95%;
+            height: 600px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <br />
+    <div class="container">
+        <span class="font-weight-bold small">Tube Well List</span>
+        <hr />
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 p-1">
+                Block              
+                    <asp:DropDownList ID="ddlBlock" runat="server" CssClass="form-control form-control-sm" AutoPostBack="true" OnSelectedIndexChanged="ddlBlock_SelectedIndexChanged">
+                        <asp:ListItem Value="0">Select</asp:ListItem>
+                    </asp:DropDownList>
+            </div>
+            <div class="col-md-2 p-1">
+                Panchyat
+                    <asp:DropDownList ID="ddlPanchayat" runat="server" CssClass="form-control form-control-sm" AutoPostBack="true" OnSelectedIndexChanged="ddlPanchayat_SelectedIndexChanged">
+                        <asp:ListItem Value="0">Select</asp:ListItem>
+                    </asp:DropDownList>
+            </div>
+            <div class="col-md-2 p-1">
+                Tubewell
+                <asp:DropDownList ID="ddlTubewell" runat="server" CssClass="form-control form-control-sm" AutoPostBack="true" OnSelectedIndexChanged="ddlTubewell_SelectedIndexChanged">
+                    <asp:ListItem Value="0">Select</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <div class="col-md-2 p-1">
+                Type
+                <asp:DropDownList ID="ddlType" runat="server" CssClass="form-control form-control-sm" AutoPostBack="true" OnSelectedIndexChanged="ddlType_SelectedIndexChanged">
+                    <asp:ListItem Value="0">Select</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <div class="col-md-2 p-1">
+                Status
+                <asp:DropDownList ID="ddlStatus" runat="server" CssClass="form-control form-control-sm" AutoPostBack="true" OnSelectedIndexChanged="ddlStatus_SelectedIndexChanged">
+                    <asp:ListItem Value="0">Select</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <div class="col-md-2 p-1">
+                <br />
+                <asp:Button ID="btnClear" runat="server" Text="Reset" CssClass="btn btn-primary btn-sm" OnClick="btnClear_Click" />
+            </div>
+            <div class="col-md-12 p-2 ">
+                <asp:GridView ID="gvTubewell" runat="server" AutoGenerateColumns="False" GridLines="None"
+                    CssClass="tableCust" EmptyDataText="No Records Found"
+                    OnPageIndexChanging="gvTubewell_PageIndexChanging" PageSize="50" AllowPaging="true" OnRowEditing="gvBlock_RowEditing" OnRowCancelingEdit="gvBlock_RowCancelingEdit" 
+                    OnRowUpdating="gvBlock_RowUpdating" >
+                    <Columns>
+                        <asp:TemplateField HeaderText="SNo">
+                            <ItemTemplate>
+                                <%#Container.DataItemIndex+1 %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="ID">
+                            <ItemTemplate>
+                                <asp:Label ID="lblID" runat="server" Text='<%# Bind("ID") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="CA Number">
+                            <ItemTemplate>
+                                <asp:Label ID="lblConsumerID" runat="server" Text='<%# Bind("ConsumerID") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Name">
+                            <ItemTemplate>
+                                <asp:Label ID="lblName" runat="server" Text='<%# Bind("Name") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="DistName">
+                            <ItemTemplate>
+                                <asp:Label ID="lblDistName" runat="server" Text='<%# Bind("DistName") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Block">
+                            <ItemTemplate>
+                                <asp:Label ID="lblBlockName" runat="server" Text='<%# Bind("BlockName") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Panchayat">
+                            <ItemTemplate>
+                                <asp:Label ID="lblPanchayatName" runat="server" Text='<%# Bind("PanchayatName") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Village">
+                            <ItemTemplate>
+                                <asp:Label ID="lblVILLNAME" runat="server" Text='<%# Bind("VILLNAME") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Type" ItemStyle-Wrap="false">
+                            <ItemTemplate>
+                                <asp:Label ID="lblType" runat="server" Text='<%# Bind("Type") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Status" ItemStyle-Wrap="false">
+                            <ItemTemplate>
+                                <asp:Label ID="lblStatus" runat="server" Text='<%# Bind("Status") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Assigned JE">
+                                    <ItemTemplate>
+                                        <asp:Label ID="lblJEName" runat="server" Text='<%# Bind("JEName") %>'></asp:Label>
+                                    </ItemTemplate>
+                                    <EditItemTemplate>
+                                        <asp:DropDownList ID="ddlJE" runat="server"></asp:DropDownList>
+                                    </EditItemTemplate>
+                                </asp:TemplateField>
+                        <asp:TemplateField HeaderText="">
+                            <ItemTemplate>
+                                <asp:Button ID="btnEdit" runat="server" Text="Assign" CommandName="Edit" class="btn btn-primary btn-sm" />
+                            </ItemTemplate>
+                            <EditItemTemplate>
+                                <asp:Button ID="btnUpdate" runat="server" Text="Update" CommandName="Update" class="btn btn-primary btn-sm" />
+                                <asp:Button ID="btnCancel" runat="server" Text="Cancel" CommandName="Cancel" class="btn btn-primary btn-sm" />
+                            </EditItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </div>
+
+        </div>
+    </div>
+
+    <asp:LinkButton ID="lnkFake" runat="server" Text=""></asp:LinkButton>
+
+</asp:Content>
+
+
+
---
+++ EE/AssignJETubewellwise.aspx.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class EE_AssignJETubewellwise : System.Web.UI.Page
+{
+    getData gd = new getData();
+    bindControls bc = new bindControls();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            bindDDLBlock();
+            bindDDLType();
+            bindDDLStatus();
+            bindgvTubewell();
+        }
+    }
+
+    
+    public void bindDDLType()
+    {
+        try
+        {
+            DataTable dt = gd.getDataTable("getTubewellType");
+            bc.bindDDL(ddlType, dt, "Type", "ID");
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    public void bindDDLStatus()
+    {
+        try
+        {
+            DataTable dt = gd.getDataTable("getTubewellStatus");
+            bc.bindDDL(ddlStatus, dt, "Status", "ID");
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+    public void bindgvTubewell()
+    {
+        try
+        {
+
+            SqlParameter[] prm = new SqlParameter[]
+                {
+                    new SqlParameter("@DistCode",Session["DistCode"].ToString()),
+                    new SqlParameter("@BlockID",ddlBlock.SelectedValue=="0"?(object)DBNull.Value:ddlBlock.SelectedValue),
+                    new SqlParameter("@PanchyatID",ddlPanchayat.SelectedValue=="0"?(object)DBNull.Value:ddlPanchayat.SelectedValue),
+                    new SqlParameter("@ID",ddlTubewell.SelectedValue=="0"?(object)DBNull.Value:ddlTubewell.SelectedValue),
+                    new SqlParameter("@Status",ddlStatus.SelectedValue=="0"?(object)DBNull.Value:ddlStatus.SelectedItem.Text),
+                    new SqlParameter("@Type",ddlType.SelectedValue=="0"?(object)DBNull.Value:ddlType.SelectedItem.Text)
+                };
+            DataTable dt = gd.getDataTable("getTubewellByDistCodeRPt", prm);
+            bc.bindGV(gvTubewell, dt);
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    public void bindDDLBlock()
+    {
+        try
+        {
+            SqlParameter[] prm = new SqlParameter[]
+                    {
+                        new SqlParameter("@DistCode",Session["DistCode"].ToString())
+                    };
+            DataTable dt = gd.getDataTable("getAllBlocksByDistCode", prm);
+            bc.bindDDL(ddlBlock, dt, "BlockName", "BlockCode");
+
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    public void bindDDLPanchyat()
+    {
+        try
+        {
+            SqlParameter[] prm = new SqlParameter[]
+                    {
+                    new SqlParameter("@BlockCode",ddlBlock.SelectedValue)
+                    };
+            DataTable dt = gd.getDataTable("getAllPanchaytByBlockCode", prm);
+            bc.bindDDL(ddlPanchayat, dt, "PanchayatName", "PanchayatCode");
+            bindDDLTubewell();
+
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+    public void bindDDLTubewell()
+    {
+        try
+        {
+            SqlParameter[] prm = new SqlParameter[]
+                    {
+                        new SqlParameter("@DistCode",Session["DistCode"].ToString()),
+                        new SqlParameter("@PanchayatCode",ddlPanchayat.SelectedValue),
+                        new SqlParameter("@BlockCode",ddlBlock.SelectedValue)
+                    };
+            DataTable dt = gd.getDataTable("getTubewellByPanchayat", prm);
+            bc.bindDDL(ddlTubewell, dt, "Name", "ID");
+
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+    protected void ddlBlock_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        bindDDLPanchyat();
+        bindgvTubewell();
+    }
+
+    protected void ddlPanchayat_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        bindDDLTubewell();
+        bindgvTubewell();
+    }
+    protected void ddlTubewell_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        bindgvTubewell();
+    }
+    protected void gvTubewell_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        bindgvTubewell();
+        gvTubewell.PageIndex = e.NewPageIndex;
+        gvTubewell.DataBind();
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        bindDDLBlock();
+        ddlPanchayat.ClearSelection();
+        ddlTubewell.ClearSelection();
+        ddlStatus.ClearSelection();
+        ddlType.ClearSelection();
+        bindgvTubewell();
+    }
+
+    protected void ddlType_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        bindgvTubewell();
+    }
+
+    protected void ddlStatus_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        bindgvTubewell();
+    }
+
+    protected void ddlDist_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        bindDDLBlock();
+        bindgvTubewell();
+    }
+    private void ExportGridToExcel(GridView gv, string filename)
+    {
+        Response.Clear();
+        Response.Buffer = true;
+        Response.ClearContent();
+        Response.ClearHeaders();
+        Response.Charset = "";
+        string FileName = filename + DateTime.Now + ".xls";
+        StringWriter strwritter = new StringWriter();
+        HtmlTextWriter htmltextwrtter = new HtmlTextWriter(strwritter);
+        gv.AllowPaging = false;
+        this.bindgvTubewell();
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.ContentType = "application/vnd.ms-excel";
+        Response.AddHeader("Content-Disposition", "attachment;filename=" + FileName);
+        gv.GridLines = GridLines.Both;
+        gv.HeaderStyle.Font.Bold = true;
+        gv.RenderControl(htmltextwrtter);
+        Response.Write(Regex.Replace(strwritter.ToString(), "</?(a|A).*?>", ""));
+        Response.End();
+
+    }
+    protected void gvBlock_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            if ((e.Row.RowState & DataControlRowState.Edit) > 0)
+            {
+                try
+                {
+                    DataRowView dr = e.Row.DataItem as DataRowView;
+                    //DropDownList ddlAE = (DropDownList)e.Row.FindControl("ddlAE");
+                    DropDownList ddlJE = (DropDownList)e.Row.FindControl("ddlJE");
+                    //SqlParameter[] prm = new SqlParameter[]{
+                    //    new SqlParameter("@DistCode",Session["DistCode"].ToString())
+                    //};
+                    SqlParameter[] prm1 = new SqlParameter[]{
+                        new SqlParameter("@DistCode",Session["DistCode"].ToString())
+                    };
+                    //DataTable dtAE = gd.getDataTable("getAEByDist", prm);
+                    //bc.bindDDL(ddlAE, dtAE, "Name", "EmpID");
+                    //ddlAE.Items.RemoveAt(0);
+                    //ddlAE.Items.Insert(0, new ListItem("Not Assigned", "0"));
+                    DataTable dtJE = gd.getDataTable("getJEByDist", prm1);
+                    bc.bindDDL(ddlJE, dtJE, "Name", "EmpID");
+                    ddlJE.Items.RemoveAt(0);
+                    ddlJE.Items.Insert(0, new ListItem("Not Assigned", "0"));
+                    //string AEEmpID = dr["AEEmpID"].ToString();
+                    string JEEmpID = dr["JEEmpID"].ToString();
+                    //if (!string.IsNullOrEmpty(AEEmpID) || AEEmpID != "0")
+                    //    ddlAE.SelectedValue = dr["AEEmpID"].ToString();
+                    if (!string.IsNullOrEmpty(JEEmpID) || JEEmpID != "0")
+                        ddlJE.SelectedValue = dr["JEEmpID"].ToString();
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+    }
+    protected void gvBlock_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        gvTubewell.EditIndex = e.NewEditIndex;
+        bindgvTubewell();
+    }
+    protected void gvBlock_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        try
+        {
+            Label lblBlockCode = gvTubewell.Rows[e.RowIndex].FindControl("lblBlockCode") as Label;
+            DropDownList ddlAE = gvTubewell.Rows[e.RowIndex].FindControl("ddlAE") as DropDownList;
+            DropDownList ddlJE = gvTubewell.Rows[e.RowIndex].FindControl("ddlJE") as DropDownList;
+            SqlParameter[] prm = new SqlParameter[]{
+                    new SqlParameter("@DistCode",Session["DistCode"].ToString()),
+                    new SqlParameter("@BlockCode",lblBlockCode.Text),
+                    new SqlParameter("@JEEmpID",ddlJE.SelectedValue),
+                    new SqlParameter("@AEEmpID",ddlAE.SelectedValue),
+                    new SqlParameter("@UpdatedByID",Session["LoginId"].ToString()),
+                    new SqlParameter("@UpdatedByIP",customVariables.GetIPAddress())
+                            };
+            gd.insExecuteSP("updBlockAEJE", prm);
+            gvTubewell.EditIndex = -1;
+            bindgvTubewell();
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+
+    protected void gvBlock_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        gvTubewell.EditIndex = -1;
+        bindgvTubewell();
+    }
+}
